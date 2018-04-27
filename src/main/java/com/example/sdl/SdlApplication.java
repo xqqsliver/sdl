d22418be3b0de190a5e5f7ec9dc228679cb75e53
xuqq <xuqq@guahao.com>
2018-04-27 10:58:02 +0800

    write your meaaage

---
+++ src/main/java/com/example/sdl/SdlApplication.java
@@ -0,0 +1,12 @@
+package com.example.sdl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdlApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/sdl/SdlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.sdl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SdlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
